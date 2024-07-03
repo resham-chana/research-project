6f538113c79ddc940da50524d2752836e196587c
resham-chana <resham.chana@hotmail.com>
2024-07-03 16:35:33 +0100

    adding tag exploration

--- millionsongs.py
+++ millionsongs.py
@@ -124,5 +124,69 @@ plt.show()
 
 ########################## clustering tags ##################################
 
-tags = lastfm_tags_df['tag'].tolist()
-tags
+import gensim.downloader as api
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+glove_model = api.load("glove-wiki-gigaword-300")
+
+glove_model["beautiful"]
+
+lastfm_df
+
+def get_tag_vector(tag, model):
+    words = tag.split()
+    vectors = [model[word] for word in words if word in model]
+    if vectors:
+        return np.mean(vectors, axis=0)
+    else:
+        return None
+    
+tags = lastfm_df.iloc[:, 1:].stack().unique()  # Get unique tags, excluding NaNs
+tag_vectors = {}
+for tag in tags:
+    if pd.notna(tag):
+        vector = get_tag_vector(tag, glove_model)
+        if vector is not None:
+            tag_vectors[tag] = vector
+
+def compute_song_vector(tags, tag_vectors):
+    vectors = []
+    for tag in tags:
+        if tag in tag_vectors:
+            vectors.append(tag_vectors[tag])
+    if vectors:
+        return np.mean(vectors, axis=0)
+    else:
+        return np.zeros(len(next(iter(tag_vectors.values()))))
+
+lastfm_df['tags'] = lastfm_df.iloc[:, 1:].apply(lambda row: row.dropna().tolist(), axis=1)
+
+lastfm_df['song_vector'] = lastfm_df['tags'].apply(lambda tags: compute_song_vector(tags, tag_vectors))
+
+song_vectors = np.stack(lastfm_df['song_vector'].values)
+
+similarity_matrix = cosine_similarity(song_vectors)
+
+similarity_df = pd.DataFrame(similarity_matrix, index=lastfm_df['tid'], columns=lastfm_df['tid'])
+print(similarity_df)
+
+########################## getting genres ##################################
+
+# get most popular tags:
+
+tag_counts = lastfm_tags_df["tag"].value_counts()
+
+for tag in tag_counts[:1000].index:
+    print(tag)
+
+tag_counts.to_csv('tags.csv', index=True)
+
+lastfm_tags_df 
+
+# unique tags:
+
+len(lastfm_tags_df["tag"].unique()) # 505215 songs with 522366 unique tags
+
+
+
