b4a75d2345c36617c97480f7e6e538d6b2b250e6
resham-chana <resham.chana@hotmail.com>
2024-07-27 00:54:33 +0100

    f

--- 5_MSD_subset_explore.py
+++ 5_MSD_subset_explore.py
@@ -3,6 +3,7 @@ import re
 import numpy as np
 import plotly.express as px
 import country_converter as coco
+import kaleido
 
 train_triplets_df = pd.read_csv(r"C:\Users\resha\data\train_triplets_df.csv")
 MSD_df = pd.read_csv(r"C:\Users\resha\data\MSD_subset.csv")
@@ -49,6 +50,7 @@ states = [('Alabama', 'AL'),('Kentucky', 'KY'),('Ohio', 'OH'),('Alaska', 'AK'), 
 
 countries = geography_df["country"]
 
+
 def determine_geo_msd(tag, states, countries):
     def space_front_word(word, text):
         pattern = r'\b' + re.escape(word) + r'\b'
@@ -73,52 +75,52 @@ def determine_geo_msd(tag, states, countries):
 # Applying the function to the DataFrame
 unique_locations = MSD_df["artist_location"].unique()
 country_mapping = {tag: determine_geo_msd(tag, states, countries) for tag in unique_locations}
-
 # Map the country back to the original DataFrame
 MSD_df['country'] = MSD_df['artist_location'].map(country_mapping)
 
 # Print the specified columns
-print(MSD_df[['country', 'artist_location']])
+print(MSD_df[['country', 'artist_location']]).drop_duplicates()
 
-unique = MSD_df.drop_duplicates(subset=['artist_location'])
-with pd.option_context('display.max_rows', None,):
-    print(unique[['artist_location', 'country']])
+none_in_country = MSD_df[MSD_df['country'].isnull()]
+missing_countries = none_in_country.drop_duplicates(subset=['artist_location'])[['artist_location', 'country']]
+missing_countries.to_csv(r"C:\Users\resha\data\missing_countries_df.csv")
+missing_countries_match = pd.read_csv(r"C:\Users\resha\data\missing_countries_match_df.csv")
 
-play_count_songs = set(play_count_grouped_df['song'])
-msd_songs = set(MSD_df['song_id'])
+try:
+    missing_countries_match = pd.read_csv(r"C:\Users\resha\data\missing_countries_match_df.csv", encoding='utf-8')
+except UnicodeDecodeError:
+    missing_countries_match = pd.read_csv(r"C:\Users\resha\data\missing_countries_match_df.csv", encoding='latin1')
 
-# Find common songs
-common_songs = play_count_songs.intersection(msd_songs)
+location_to_country_dict = missing_countries_match.set_index('artist_location')['country'].to_dict()
+MSD_df['country'] = MSD_df['country'].fillna(MSD_df['artist_location'].map(location_to_country_dict))
 
+
+# merge dataset with genre and playcount
 MSD_merged_df = pd.merge(MSD_df, play_count_grouped_df.iloc[:,[1,2]], left_on='song_id', right_on='song').drop('song', axis=1)
-MSD_merged_df = pd.merge(MSD_merged_df, genres_df.iloc[:,[1,2]], how='inner', on='track_id')
+MSD_merged_df_genre = pd.merge(MSD_merged_df, genres_df.iloc[:,[1,2]], how='inner', on='track_id')
 MSD_merged_df['log_total_play_count'] = np.log10(MSD_merged_df['total_play_count'])
 
 MSD_df.columns
-checkrows = MSD_merged_df.dropna(subset="country")
-MSD_grouped_df = MSD_merged_df[["country","log_total_play_count"]].groupby('country', as_index=False).mean()
+checkrows = MSD_df.dropna(subset="country")
+MSD_grouped_df = MSD_merged_df[["country","log_total_play_count"]].groupby('country', as_index=False).sum()
 MSD_grouped_df = MSD_grouped_df.dropna(subset="country")
 
-MSD_grouped_df['iso_alpha_3'] = coco.convert(names=MSD_grouped_df['nationalities'], to='ISO3')
-track_features_country_grouped_df = MSD_grouped_df[['log_total_play_count', 'iso_alpha_3',"country"]]
+MSD_grouped_df['iso_alpha_3'] = coco.convert(names=MSD_grouped_df['country'], to='ISO3')
+MSD_grouped_df = MSD_grouped_df[['log_total_play_count', 'iso_alpha_3',"country"]]
 #unique_nationalities = MSD_grouped_df['country'].unique()
 
-track_features_country_df.sort_values(by="artist_familiarity", ascending=False)
-track_features_country_grouped_df.sort_values(by="artist_familiarity", ascending=False)
-track_features_country_df[track_features_country_df["nationalities"] == "luxembourg"]
 
 
-plt.style.use('dark_background')
 # Create basic choropleth map
 fig = px.choropleth(
-    track_features_country_grouped_df,
+    MSD_grouped_df,
     locations='iso_alpha_3',
-    color='artist_familiarity',
+    color='log_total_play_count',
     color_continuous_scale='viridis',  
     template="plotly_dark",# Color scale
-    title='Total Play Count by Country',
-    labels={'total_play_count': 'Total Play Count'},
-    hover_name='nationalities'  # Show country names on hover
+    title='Total Log Play Count by Country',
+    labels={'log_total_play_count': 'Total Play Count'},
+    hover_name='country'  # Show country names on hover
 )
 fig.write_image("log_total_play_count_map.png")
 fig.show()
