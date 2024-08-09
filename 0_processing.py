17c5c6973ce1ef42811c888466e07f41bf155583
resham-chana <resham.chana@hotmail.com>
2024-08-09 22:11:24 +0100

    changes to comments

--- 0_processing.py
+++ 0_processing.py
@@ -1,8 +1,6 @@
 # importing relevant libraries
-import numpy as np
 import sqlite3
 import pandas as pd
-import matplotlib.pyplot as plt
 import requests
 from pathlib import Path
 
@@ -13,12 +11,6 @@ triplets_path = r'C:\Users\resha\data\train_triplets.txt'
 genre_path = r'C:\Users\resha\data\msd-MAGD-genreAssignment.cls'
 image_path = r'C:\Users\resha\data\MSD-I_dataset.tsv'
 geography_path = r'C:\Users\resha\data\countries.csv'
-#db_track_path = r'C:\Users\corc4\Downloads\track_metadata.db'
-#db_tag_path = r'C:\Users\corc4\Downloads\lastfm_tags.db'
-#triplets_path = r"C:\Users\corc4\Downloads\train_triplets.txt"
-#genre_path = r'C:\Users\corc4\Downloads\msd-MAGD-genreAssignment.cls'
-#image_path = r'C:\Users\corc4\Downloads\MSD-I_dataset.tsv'
-#geography_path = r'C:\Users\corc4\Downloads\countries.csv'
 
 # read the country data, keep relevant columns and save as a csv
 geography_df = pd.read_csv(geography_path)
@@ -38,23 +30,19 @@ dict = {'Country': 'country',
 geography_df.rename(columns=dict,
           inplace=True)
 
-#geography_df.to_csv(r"C:\Users\resha\data\geography_df.csv")  
-geography_df.to_csv(r"C:\Users\corc4\data\geography_df.csv")  
+geography_df.to_csv(r"C:\Users\resha\data\geography_df.csv")  
 
 # read train_triplet text file and add headers
 train_triplets_df = pd.read_table(triplets_path, sep='\t', header=None, names=['user', 'song', 'play_count'])
 print(train_triplets_df.head())
-# group to find total play count
-# Step 2: Group by song and sum the play counts, then filter and sort
 
+# group to find total play count
 play_count_grouped_df = train_triplets_df.groupby('song', as_index=False)['play_count'].sum().rename(columns={'play_count': 'total_play_count'})
 play_count_grouped_df = play_count_grouped_df.sort_values(by='total_play_count', ascending=False)
 
 # write grouped play count data and triplets to csv
 train_triplets_df.to_csv(r"C:\Users\resha\data\train_triplets_df.csv")  
-#train_triplets_df.to_csv(r"C:\Users\corc4\data\train_triplets_df.csv")  
 play_count_grouped_df.to_csv(r"C:\Users\resha\data\play_count_grouped_df.csv")  
-#play_count_grouped_df.to_csv(r"C:\Users\corc4\data\play_count_grouped_df.csv")  
 
 # housekeeping: check column names, unique tracks and nulls
 train_triplets_df
@@ -63,7 +51,7 @@ train_triplets_df.columns
 len(pd.unique(train_triplets_df['track_id']))
 print(train_triplets_df.isnull().sum().sum())
 
-# Read the image dataset:
+# read the image dataset:
 images_df = pd.read_csv(image_path, sep='\t')
 images_df.head()
 
@@ -74,7 +62,6 @@ images_df['msd_track_id'].nunique()
  
 # write to csv
 images_df.to_csv(r"C:\Users\resha\data\images_df.csv")  
-#images_df.to_csv(r"C:\Users\corc4\data\images_df.csv")  
 
 # Function to download an image from a URL
 def download_image(url, path):
@@ -85,19 +72,19 @@ def download_image(url, path):
             for chunk in response.iter_content(1024): 
                 file.write(chunk)
     except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred while downloading {url}: {http_err}")
+        print(f"error: {http_err}")
     except requests.exceptions.ConnectionError as conn_err:
-        print(f"Connection error occurred while downloading {url}: {conn_err}")
+        print(f"error: {conn_err}")
     except requests.exceptions.Timeout as timeout_err:
-        print(f"Timeout error occurred while downloading {url}: {timeout_err}")
+        print(f"error: {timeout_err}")
     except requests.exceptions.RequestException as req_err:
-        print(f"An error occurred while downloading {url}: {req_err}")
+        print(f"error: {req_err}")
     except Exception as e:
-        print(f"An unexpected error occurred while downloading {url}: {e}")
+        print(f"error: {e}")
 
 # Base directory for the images 
-#base_dir = 'C:\Users\resha\images'
-#base_dir = r'C:\Users\corc4\Downloads\images'
+base_dir = 'C:\Users\resha\images'
+
 # Set to keep track of downloaded URLs
 downloaded_urls = set()
 
@@ -106,23 +93,16 @@ for index, row in images_df.iterrows():
     set_name = row['set']
     genre = row['genre']
     url = row['image_url']
-    
-    # skips the duplicates is already downloaded
+    # skips the duplicates if already downloaded
     if url in downloaded_urls:
         print(f"Skipping already downloaded URL: {url}")
         continue
-    
     # create directory
     dir_path = Path(base_dir) / set_name / genre
     dir_path.mkdir(parents=True, exist_ok=True)
-    
     # Define the image path
-    image_path = dir_path / f"{row['msd_track_id']}.jpg"
-    
-    # Download the image
+    image_path = dir_path / f"{row['msd_track_id']}.jpg"    
     download_image(url, image_path)
-    
-    # Add the URL to the set of downloaded URLs
     downloaded_urls.add(url)
 
 # code to covert the track metadata database into a pandas data frame
@@ -139,7 +119,6 @@ conn.close()
 
 # write track metadata to csv
 track_metadata_df.to_csv(r"C:\Users\resha\data\track_metadata_df.csv")  
-#track_metadata_df.to_csv(r"C:\Users\corc4\data\track_metadata_df.csv")  
 
 # housekeeping: check columns, unique tracks and nulls
 track_metadata_df
@@ -148,7 +127,7 @@ track_metadata_df.columns
 len(pd.unique(track_metadata_df['track_id']))
 print(track_metadata_df.isnull().sum().sum())
 
-# converting last.fm tag dataset to a pandas datafram
+# converting last.fm tag dataset to a pandas dataframe
 conn_tag = sqlite3.connect(db_tag_path)
 # get data of tags and track ID
 q_tag = """
@@ -176,8 +155,6 @@ lastfm_pivot_df.columns = ['tid'] + [f'tag{i}' for i in range(1, len(lastfm_pivo
 # write csv
 lastfm_tags_df.to_csv(r"C:\Users\resha\data\lastfm_tags_df.csv")  
 lastfm_pivot_df.to_csv(r"C:\Users\resha\data\lastfm_pivot_df.csv")  
-#lastfm_tags_df.to_csv(r"C:\Users\corc4\data\lastfm_tags_df.csv")  
-#lastfm_pivot_df.to_csv(r"C:\Users\corc4\data\lastfm_pivot_df.csv") 
 
 # opening genre dataset and coverting to dataframe 
 genres_df = pd.read_csv(genre_path,delimiter="\t", header=None)
@@ -186,7 +163,6 @@ print(genres_df)
 genres_df["track_id"].nunique
 
 genres_df.to_csv(r"C:\Users\resha\data\genres_df.csv")  
-#genres_df.to_csv(r"C:\Users\corc4\data\genres_df.csv")  
 
 lastfm_pivot_df #all tags for each unique song nrow: 505216
 lastfm_tags_df # song list with tags that are not unique: 8598630
